3ccd62c7c3bb24b62c78c1ac0fbcb7567a7382e0
HameemH <hameemhossain2@gmail.com>
2022-05-14 08:14:59 +0600

    services open for appointment api created

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ld9mo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const collection = client.db("doctorPortal").collection("services");
+       app.get('/services', async(req,res)=>{
+           const query ={};
+           const cursor =collection.find(query);
+           const services = await cursor.toArray();
+           res.send(services)
+           console.log(services);
+       })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
